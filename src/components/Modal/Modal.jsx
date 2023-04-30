a775af0a418cef2f95d00626c01adfd58753d999
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-04-30 18:22:48 +0300

    final dev

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom'; // для рендеринга в іншому місці
+import css from './Modal.module.css';
+
+// Пошук модалки щоб динамічно додати до DOM-дерева сторінки
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  // реєструє обробник події keydown на вікні браузера
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyDown); // при натисканні клавіші Escape викликає функцію keyDown
+  }
+
+  keyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.keyDown); // видаляє обробник події keydown з вікна браузера
+  }
+
+  // закриття модалки по кліку на бекдроп
+  handleClose = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div onClick={this.handleClose} className={css.Overlay}>
+        <div className={css.Modal}>{this.props.children}</div>{' '}
+        {/* рендеринг дочірніх елементів */}
+      </div>,
+      modalRoot
+    );
+  }
+}
